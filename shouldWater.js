96ce1b9f4d3e01d08f60430c04364b7d9e4fc79b
Nirojan Kalaimannan <nirokal92@gmail.com>
2022-07-19 18:56:07 -0400

    Add files via upload

---
+++ shouldWater.js
@@ -0,0 +1,5 @@
+const argumentValue = Number(process.argv[2]);
+const argumentValue2 = Number(process.argv[3]);//second argument for commandline
+
+if(argumentValue==0 && argumentValue2>10)
+console.log('WATER');
